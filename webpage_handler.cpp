927f5d10a7f3e595716dff600e6840d206f3c2a8
Luckless <admin@luckless.pw>
2025-06-12 14:34:23 +0700

    Enhances build and MIME type handling
    Refactors the build process for improved cross-platform compatibility, specifically for Windows using MSYS2.
    Includes the retrieval and handling of MIME types from a CSV file at build time, embedding them into the executable.
    This ensures correct content types are served for web resources.

--- webpage_handler.cpp
+++ webpage_handler.cpp
@@ -1,13 +1,43 @@
 #include "webpage_handler.h"
+#include "mime_types_data.h"
 
 #include <string>
 #include <fstream>
+#include <unordered_map>
 #include <filesystem>
+
+std::string content_type(const std::string &file_extension) {
+    std::unordered_map<std::string, std::string> mime_types;
+
+    std::istringstream csv_stream(embedded::MIME_TYPES_CSV);
+    std::string line;
+
+    std::getline(csv_stream, line);
+
+    while (std::getline(csv_stream, line)) {
+        std::stringstream line_stream(line);
+        std::string extension, mime_type;
+
+        std::getline(line_stream, extension, ',');
+        std::getline(line_stream, mime_type, ',');
+
+        mime_types[extension] = mime_type;
+    }
+
+    std::string ext;
+
+    if (ext = file_extension; !ext.empty() && ext[0] == '.') {
+        ext = ext.substr(1);
+    }
+
+    return mime_types.contains(ext) ? mime_types[ext] : "application/octet-stream";
+}
 
 std::string webpage_handler(
     const std::string &url
 ) {
     std::string mutable_url = url;
+    auto extension = std::filesystem::path(mutable_url).extension().string();
 
     if (mutable_url == "/") {
         mutable_url = "/index.html";
@@ -26,13 +56,13 @@ std::string webpage_handler(
     if (!content.is_open()) {
         if (std::ifstream fallback_content("www/404.html"); fallback_content.is_open()) {
             return "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" +
-                   std::string(std::istreambuf_iterator<char>(fallback_content), std::istreambuf_iterator<char>());
-        } else {
-            return "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" +
-                   std::string(
-                       R"(<html><body style="background-color: black; margin: 0; display: flex; justify-content: center; align-items: center; height: 100vh;"><div style="text-align: center;"><h1 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white;">404</h1><p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white;">Page Not Found</p></div><p style="position: absolute; bottom: 0; left: 50%; transform: translateX(-50%); padding: 10px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white;">Powered by Jella Web Server</p></body></html>)");
+                   std::string(std::istreambuf_iterator(fallback_content), std::istreambuf_iterator<char>());
         }
+
+        return "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" +
+               std::string(
+                   R"(<html><body style="background-color: black; margin: 0; display: flex; justify-content: center; align-items: center; height: 100vh;"><div style="text-align: center;"><h1 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white;">404</h1><p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white;">Page Not Found</p></div><p style="position: absolute; bottom: 0; left: 50%; transform: translateX(-50%); padding: 10px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color: white;">Powered by Jella Web Server</p></body></html>)");
     }
-    return "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" +
+    return "HTTP/1.1 200 OK\r\nContent-Type: " + content_type(extension) + "\r\n\r\n" +
            std::string(std::istreambuf_iterator<char>(content), std::istreambuf_iterator<char>());
 }
